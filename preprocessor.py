1c268b199e8b23648e72e1bdf3494f288ec9350c
unknown <mananchahal01@gmail.com>
2026-01-30 23:28:59 +0530

    Completed the project

--- preprocessor.py
+++ preprocessor.py
@@ -2,41 +2,66 @@ import pandas as pd
 import re
 
 def preprocessor(data):
-    date_pattern = "\[\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{1,2}:\d{1,2}\s(?:PM|AM)\]\s"
+    date_pattern = r"\[\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{1,2}:\d{1,2}\s(?:PM|AM)\]\s"
 
-    messages = re.split(date_pattern,data)[1:]
-    dates = re.findall(date_pattern,data)
+    messages = re.split(date_pattern, data)[1:]
+    dates = re.findall(date_pattern, data)
     dates = [d.replace('\u202f', ' ') for d in dates]
 
-    df = pd.DataFrame({'user_message':messages,'message_date':dates})
+    df = pd.DataFrame({
+        'user_message': messages,
+        'message_date': dates
+    })
 
-    df['message_date'] = df['message_date'].str.strip().str.strip('[]')
+    df['message_date'] = (
+        df['message_date']
+        .str.strip()
+        .str.strip('[]')
+    )
 
     df['message_date'] = pd.to_datetime(
-                                    df['message_date'],
-                                    format='%d/%m/%y, %I:%M:%S %p'
-                                    )
+        df['message_date'],
+        format='%d/%m/%y, %I:%M:%S %p'
+    )
+
     
-    users=[]
-    messages=[]
+    df.rename(columns={'message_date': 'date'}, inplace=True)
+
+    users = []
+    messages = []
 
     for msg in df['user_message']:
-        entry = re.split('([\w\W]+?):\s',msg)
+        entry = re.split(r'([\w\W]+?):\s', msg)
         if entry[1:]:
             users.append(entry[1])
-            messages.append(entry[2])
+            messages.append(" ".join(entry[2:]))
         else:
-            users.append('group notification')
+            users.append('group_notification')
             messages.append(entry[0])
 
     df['user'] = users
     df['message'] = messages
+    df.drop(columns=['user_message'], inplace=True)
 
-    df.drop(columns=['user_message'],inplace=True)
+    # 🔥 ADD ALL MISSING COLUMNS
+    df['only_date'] = df['date'].dt.date
+    df['year'] = df['date'].dt.year
+    df['month_num'] = df['date'].dt.month
+    df['month'] = df['date'].dt.month_name()
+    df['day'] = df['date'].dt.day
+    df['day_name'] = df['date'].dt.day_name()
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
 
-    df['month'] = df['message_date'].dt.month_name()
-    df['day'] = df['message_date'].dt.day
-    df['hour'] = df['message_date'].dt.hour
-    df['minute'] = df['message_date'].dt.minute
+    period = []
+    for hour in df['hour']:
+        if hour == 23:
+            period.append("23-00")
+        elif hour == 0:
+            period.append("00-01")
+        else:
+            period.append(f"{hour}-{hour+1}")
+
+    df['period'] = period
 
     return df
